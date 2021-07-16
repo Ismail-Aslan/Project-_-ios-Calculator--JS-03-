a876e94eba005e5780174ba60accee28a4ed706a
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-07-17 00:40:40 +0300

    operators get functionality

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const btnNum = document.querySelectorAll(".btn--num");
 const displayScreen = document.querySelector("#displayScreen");
 const operators = document.querySelectorAll(".btn--sign");
 
-var result;
-var num;
-let operant = false;
+let result = Number(displayScreen.innerText);
+let previousNum,nextNum;
+let counter = 0,
+operant = true,
+operator = "";
+
 console.log(operators[0].innerText);
 console.log(operators[1].innerText);
 console.log(operators[2].innerText);
@@ -160,47 +163,152 @@ btnNum[10].addEventListener('click',function () {
         
         // console.log(displayScreen.innerText);
     } else {
-        displayScreen.innerHTML += "<h2 style='display:inline;'>,</h2>";
+        displayScreen.innerHTML += "<h2 style='display:inline;'>.</h2>";
         // console.log(displayScreen.innerText);
     }
 });
 
+
+
+
+
+function operate(x,y,operator) {
+    if (operator === "/") {
+        if (y === 0) {
+            result = "Error";
+        }else {
+            result = x / y;
+        }
+    }
+    if (operator === "*") {
+        result = x * y;
+    }
+    if (operator === "-") {
+        result = x - y;
+    }
+    if (operator === "+") {
+        result = x + y;
+    }
+    operator="";
+}
+function showResult() {
+    console.log(result,"  res else");
+    displayScreen.innerHTML = `<h2 style='display:inline;'>${result}</h2>`;
+    previousNum = result;
+    console.log(previousNum,"  prv else");
+}
+
+
+
+
 operators[0].addEventListener('click',() => {
-    // if (operant) {
-    //     result = undefined;
-    //     operant = false;
-    // }
     operant = true;
-    num = Number(displayScreen.innerText);
-    if (result === undefined) {
-        result = num;
-        console.log(num);
+    if (counter < 1) {
+        result = Number(displayScreen.innerText);
+        previousNum = result;
+        operator = "/";
+        console.log(previousNum," prv if");
     } else {
-        if (result === 0) {
-            num = undefined;
-            displayScreen.innerHTML = `<h2 style='display:inline;'>Error</h2>`;
+        if (operator === "") {
+            operator = "/"
+            // showResult();
         }else{
-            result = Number(result) / num;
-            console.log(result);
-            displayScreen.innerHTML = `<h2 style='display:inline;'>${result}</h2>`;
+            result = Number(displayScreen.innerText);
+            nextNum = result;
+            console.log(nextNum,"  nxt else");
+
+            operate(previousNum,nextNum,operator);
+            operator = "/"
+            showResult();
+        }
+    }
+    counter++;
+});
+
+operators[1].addEventListener('click',() => {
+    operant = true;
+    if (counter < 1) {
+        result = Number(displayScreen.innerText);
+        previousNum = result;
+        operator = "*";
+        console.log(previousNum," prv if");
+    } else {
+        if (operator === "") {
+            operator = "*"
+            // showResult();
+        }else {
+            result = Number(displayScreen.innerText);
+            nextNum = result;
+            console.log(nextNum,"  nxt else");
             
+            operate(previousNum,nextNum,operator);
+            operator = "*"
+            showResult();
+        }
+    }
+    counter++;
+});
+
+operators[2].addEventListener('click',() => {
+    operant = true;
+    if (counter < 1) {
+        if (operator === "") {
+            operator = "/"
+            // showResult();
+        }
+        result = Number(displayScreen.innerText);
+        previousNum = result;
+        operator = "-";
+        console.log(previousNum," prv if");
+    } else {
+        if (operator === "") {
+            operator = "-"
+            // showResult();
+        } else{
+            result = Number(displayScreen.innerText);
+            nextNum = result;
+            console.log(nextNum,"  nxt else");
             
+            operate(previousNum,nextNum,operator);
+            operator = "-"
+            showResult();
+        }
+    }
+    counter++;
+});
+
+operators[3].addEventListener('click',() => {
+    operant = true;
+    if (counter < 1) {
+        result = Number(displayScreen.innerText);
+        previousNum = result;
+        operator = "+";
+        console.log(previousNum," prv if");
+    } else {
+        if (operator === "") {
+            operator = "+"
+            // showResult();
+        }else{
+            result = Number(displayScreen.innerText);
+            nextNum = result;
+            console.log(nextNum,"  nxt else");
+
+            operate(previousNum,nextNum,operator);
+            operator = "+"
+            showResult();
         }
         
     }
+    counter++;
 });
-operators[1].addEventListener('click',() => {
-    
+
+operators[4].addEventListener('click',() => {
     operant = true;
-    num = Number(displayScreen.innerText);
-    if (result === undefined) {
-        result = num;
-    } else {
-      
-        result = Number(result)*num;
-        console.log(result);
-        displayScreen.innerHTML = `<h2 style='display:inline;'>${result}</h2>`;
-        result = undefined;
-        
-    }
+    result = Number(displayScreen.innerText);
+    nextNum = result;
+    console.log(nextNum,"  nxt else");
+    operate(previousNum,nextNum,operator);
+    showResult();
+    operator="";
+    counter++;
 });
